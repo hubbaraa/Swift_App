a10bbd5e2b76da661107f14846c411204d6786c6
Aaron Hubbard <hubbaraa@gmail.com>
2017-06-19 17:55:31 -0700

    My Stage Point

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Aaron Hubbard on 6/19/17.
+//  Copyright © 2017 Aaron Hubbard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 10 {
+            theLabel.text = "You Tapped the button 10 times!"
+        }
+    }
+    
+    @IBAction func buttonPush2(_ sender: Any) {
+        theLabel.text = "Buttons Are Cool!"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
